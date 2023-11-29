9adf99408e4d06f45c67ad38d45ad5b3e0735866
Anna_Rezvova <rezvova.a@mail.ru>
2023-11-29 19:10:45 +0500

    Update test

--- test_positive.py
+++ test_positive.py
@@ -5,16 +5,6 @@ with open('config.yaml') as f:
     data = yaml.safe_load(f)
 
 
-def test_create_post(token, text_title, text_description, text_content):
-    requests.post(url=data['post_site'], headers={"X-Auth-Token": token},
-                 params={"title": text_title, "description": text_description,
-                         "content": text_content})
-    res_get = requests.get(url=data['post_site'], headers={"X-Auth-Token": token},
-                           params={"owner": 'Me'})
-    description_list = [i['description'] for i in res_get.json()['data']]
-    assert text_description in description_list
-
-
 
 def test_create_post(token, text_title, text_description, text_content):
     # Проверка создания поста
